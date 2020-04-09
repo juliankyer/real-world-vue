b449efbf5a8345609b260b822224e0caec436954
Julian Kyer <julian.kyer@gmail.com>
2020-04-09 16:12:45 -0600

    start error messages

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -24,20 +24,43 @@ export const mutations = {
 };
 
 export const actions = {
-  createEvent({ commit }, event) {
-    return EventService.postEvent(event).then(() => commit('ADD_EVENT', event));
+  createEvent({ commit, dispatch }, event) {
+    return EventService.postEvent(event)
+      .then(() => {
+        commit('ADD_EVENT', event);
+
+        const notification = {
+          type: 'success',
+          message: 'Event added successfully',
+        };
+
+        dispatch('notification/add', notification, { root: true });
+      })
+      .catch(error => {
+        const notification = {
+          type: 'error',
+          message: 'There was a problem adding event: ' + error.message,
+        };
+
+        dispatch('notification/add', notification, { root: true });
+        throw error;
+      });
   },
-  fetchEvents({ commit }, { perPage, page }) {
+  fetchEvents({ commit, dispatch }, { perPage, page }) {
     EventService.getEvents(perPage, page)
       .then(response => {
         commit('SET_TOTAL_EVENTS', response.headers['x-total-count']);
         commit('SET_EVENTS', response.data);
       })
       .catch(error => {
-        console.log('Error:', error.response);
+        const notification = {
+          type: 'error',
+          message: 'There was a problem fetching events: ' + error.message,
+        };
+        dispatch('notification/add', notification, { root: true });
       });
   },
-  fetchEvent({ commit, getters }, id) {
+  fetchEvent({ commit, getters, dispatch }, id) {
     var event = getters.getEventById(id);
 
     if (event) {
@@ -48,7 +71,11 @@ export const actions = {
           commit('SET_EVENT', response.data);
         })
         .catch(error => {
-          console.log('Error:', error.response);
+          const notification = {
+            type: 'error',
+            message: 'There was a problem fetching event: ' + error.message,
+          };
+          dispatch('notification/add', notification, { root: true });
         });
     }
   },
